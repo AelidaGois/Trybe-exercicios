1f694b5c602c883cb78f374fbbbbdeb3f56ce222
Aelida <lidasouza89@gmail.com>
2022-04-08 16:19:05 -0300

    Inclusão da resolução do exercício/ex-fix-4.js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-fix-4.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var soma = 0;
+var mediaAritmetica = 0;
+for (let index = 0; index < numbers.length; index++) {
+    soma += numbers[index];
+    mediaAritmetica = (soma / numbers.length)
+    
+}
+
+if (mediaAritmetica > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
