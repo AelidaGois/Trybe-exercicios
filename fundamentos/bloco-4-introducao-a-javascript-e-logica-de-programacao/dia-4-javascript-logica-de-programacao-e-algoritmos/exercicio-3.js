8a413881c75931f87acb4cb39b5c9b36bcff73b6
Aelida <lidasouza89@gmail.com>
2022-04-11 18:44:29 -0300

    Inclusão do exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-logica-de-programacao-e-algoritmos/exercicio-3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-logica-de-programacao-e-algoritmos/exercicio-3.js
@@ -7,7 +7,7 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim'
-  }
+  };
 
   for(let dados in info){
       console.log(dados);
