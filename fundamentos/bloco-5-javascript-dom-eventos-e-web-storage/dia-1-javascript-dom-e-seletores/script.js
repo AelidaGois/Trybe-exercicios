b60078ba5bafcdb074b2537d1b65c9c2a573ba07
Aelida <lidasouza89@gmail.com>
2022-04-18 10:07:41 -0300

    alterações

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,3 +1,5 @@
 document.getElementsByTagName("p")[0].innerHTML = "Daqui 2 anos me vejo uma pessoa trilhando na carreira de desenvolvedora web, inclusive atuante em alguma empresa buscando dar sempre o meu melhor para o meu crescimento pessoal e profissional.";
 
 document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
+
+document.getElementsByClassName(".main-content .center-content").style.backgroundColor = "white";
