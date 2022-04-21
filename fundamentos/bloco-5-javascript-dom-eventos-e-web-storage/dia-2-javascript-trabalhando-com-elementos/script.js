5efd48ce91f3643996c24068c5dfb853ee5dcf8b
Aelida <lidasouza89@gmail.com>
2022-04-21 13:25:22 -0300

    Inclusão

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,10 @@
+// 1. Onde eu quero adicionar os elementos?
+let container = document.getElementById("first");
+
+// 2. Crio um elemento HTML via JS
+let newDiv = document.createElement('div');
+
+newDiv.className = 'red circle small';
+
+
+// 3. Adiciono o elemento criado na estrutura HTML 
