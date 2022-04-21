f0d6ec19029720995af2d2c26dcc0b3e65faae92
Aelida <lidasouza89@gmail.com>
2022-04-21 13:36:32 -0300

    Inclusão Resolução exercício 5 - Exercícios de Fixação

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-fix-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-fix-2.js
@@ -2,6 +2,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 var soma = 0;
 for(let index = 0; index < numbers.length;index++){
-soma += numbers[index];
+soma+= numbers[index];
 }
 console.log(soma);
