bd3cde3c8bf63b98d37669f9437200650e45fbe7
Aelida <lidasouza89@gmail.com>
2022-04-21 17:02:14 -0300

    revisão

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-logica-de-programacao-e-algoritmos/exercicio-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-logica-de-programacao-e-algoritmos/exercicio-2.js
@@ -4,7 +4,7 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
+    recorrente: 'Sim',
   };
 
   console.log(info.recorrente);
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-logica-de-programacao-e-algoritmos/exercicio-4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-logica-de-programacao-e-algoritmos/exercicio-4.js
@@ -4,7 +4,6 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim'
   };
 
