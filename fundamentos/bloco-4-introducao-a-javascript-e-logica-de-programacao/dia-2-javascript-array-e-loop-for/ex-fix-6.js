549882e8d507559eac8b38f15b688d89824e92da
Aelida <lidasouza89@gmail.com>
2022-04-21 16:06:25 -0300

    Inclusão dos enunciados em forma de comentário no vs code e inclusão resolução exercicio 8 e 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-fix-6.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-fix-6.js
@@ -1,3 +1,6 @@
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let impar = [];
@@ -7,11 +10,11 @@ for (let index = 0; index < numbers.length; index++) {
     if (numbers[index] % 2 !== 0) {
         impar.push(numbers[index]);
         cont++;
-        
+
 
     } else if (numbers % 2 === 0) {
         console.log("nenhum valor ímpar encontrado");
     }
-
-} console.log("Números ímpares: " + impar + " Quantidade de números ímpares: " + cont);
+}
+console.log("Números ímpares: " + impar + " Quantidade de números ímpares: " + cont);
 
